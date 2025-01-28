6be01c38a8ee711ef787338234c5579440bd1d7d
Ian Ross <iross@cs.wisc.edu>
2025-01-28 16:17:56 +0000

    initcommit

---
+++ is_cat.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+#%HTCSS TEMPLATE
+#executable=$(App)
+#arguments= $(Image) $(Prediction)
+#error=#(Error)
+#log=is_cat.log
+#request_cpus=$(Core)
+#request_memory=$(Memory)
+#transfer_output_files=cat_detection_model.pth
+#request_disk=$(DiskSpace)
+#queue from TABLE table.csv
+#%HTCSS TABLE
+# jobN, Image, Prediction, Error, App, Core, Memory, DiskSpace
+# 1, img001, img001_result.txt, 1.err, is_cat.py, 1, 2GB, 1GB
+# 2, img002, img002_result.txt, 2.err, is_cat.py, 1, 2GB, 1GB
+# 3, img003, img003_result.txt, 3.err, is_cat.py, 1, 2GB, 1GB
+# 4, img004, img004_result.txt, 4.err, is_cat.py, 1, 2GB, 1GB
+# 5, img005, img005_result.txt, 5.err, is_cat.py, 1, 2GB, 1GB
+# 6, img006, img006_result.txt, 6.err, is_cat.py, 1, 2GB, 1GB
+#%HTCSS END
+
+import torch
+import glob
+
+def main():
+    input_name, output_name = sys.argv[1:3]
+    model = torch.load('cat_detection_model.pth')
+    model.eval()
+    is_cat(model, input_name, output_name)
+
+def is_cat(model, image, result):
+    image_tensor = torch.from_numpy(image).float().unsqueeze(0)
+    output = model(image_tensor)
+    cat_prob = torch.nn.functional.softmax(output, dim=-1)
+    with open(output_name) as fout:
+        fout.write(cat_prob)
+    return 0
+
+def train(images, labels):
+    # generate a dummy model for detecting cats in an image
+    model = torch.nn.Sequential(
+        torch.nn.Conv2d(1, 10, kernel_size=5),
+        torch.nn.ReLU(),
+        torch.nn.MaxPool2d(2),
+        torch.nn.Flatten(),
+        torch.nn.Linear(320, 50),
+        torch.nn.ReLU(),
+        torch.nn.Linear(50, 2)
+    )
+    # Load data and train the model
+    data_loader = torch.utils.data.DataLoader(
+        list(zip(images, labels)), batch_size=64, shuffle=True
+    )
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    for epoch in range(10):  # number of epochs
+        for batch_images, batch_labels in data_loader:
+            optimizer.zero_grad()
+            outputs = model(batch_images)
+            loss = criterion(outputs, batch_labels)
+            loss.backward()
+            optimizer.step()
+
+
+if __name__ == "__main__":
+    main()
---
+++ parse.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+import os
+import tokenize
+import click
+import htcondor2 as htcondor
+
+def read_comments(fileObj: bytes):
+    comments = ''
+    for toktype, tok, start, end, line in tokenize.tokenize(fileObj.readline):
+        # we can also use token.tok_name[toktype] instead of 'COMMENT'
+        # from the token module 
+        if toktype == tokenize.COMMENT and line.startswith("#"):
+            if line[0] == "#": # TODO: make this less bad.
+                line = line[1:] 
+            comments+=line
+    return comments
+
+PATTERN = "%HTCSS"
+ENDPATTERN = "%HTCSS END"
+
+def parse_htcss_string(text: str) -> dict:
+    text = [i+"\n" for i in text.split("\n")]
+    result = {}
+    in_block = False
+    current_block_name = None
+    block_contents = []
+    for line in text:
+        if line.startswith(PATTERN):
+            if in_block:
+                result[current_block_name] = "".join(block_contents).strip()
+                block_contents = []
+            current_block_name = line.split()[1]
+            in_block = True
+        elif in_block:
+            block_contents.append(line)
+        elif line.startswith(ENDPATTERN):
+            in_block = False
+            break
+    if in_block:
+        result[current_block_name] = "".join(block_contents).strip()
+    if not result["TEMPLATE"] or not result["TABLE"]:
+        raise Exception("Missing template or table in submission file")
+    # TODO: Check for validity of submit template and/or input tables.
+    return result
+
+def parse_htcss_file(file): 
+    """Parse a txt file, looking for lines that start with %HTCSS and extract
+    the next word in that line. All of the text until the next line that starts
+    with %HTCSS should be captures
+    """
+    with open(file) as f:
+        lines = f.read()
+    return parse_htcss_string(lines)
+
+def write_table(table):
+    with open("_table.csv", "w") as f:
+        f.write(table)
+    return 0
+
+
+@click.command()
+@click.option("--executable", help="Treat the input file as an executable and extract from comments", is_flag=True, default=False)
+@click.option("--cleanup", help="Cleanup after running", is_flag=True, default=False)
+@click.option("--dryrun", help="Don't submit.", is_flag=True, default=False)
+@click.argument('file', type=click.Path(exists=True))
+def main(file, executable, cleanup, dryrun):
+    if executable:
+        with open(file, "rb") as file:
+            res = parse_htcss_string(read_comments(file))
+    else: 
+        res = parse_htcss_file(file)
+    write_table(res["TABLE"])
+    schedd = htcondor.Schedd()
+    try:
+        if dryrun:
+            print(htcondor.Submit(res["TEMPLATE"]))
+            print(htcondor.Submit(res["TEMPLATE"]).getQArgs())
+        else:
+            submit_result = schedd.submit(htcondor.Submit(res["TEMPLATE"]))
+            print(submit_result)
+    except TypeError:
+        import pdb; pdb.set_trace()
+    if cleanup:
+        os.remove("_table.csv")
+
+if __name__ == "__main__":
+    main()
